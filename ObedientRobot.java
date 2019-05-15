06fccf2306ac0a49c09a2bdaf1d9035b0cc2d88f
995alex995 <49413189+995alex995@users.noreply.github.com>
2019-05-16 00:37:51 +0200

    Add files via upload

---
+++ ObedientRobot.java
@@ -0,0 +1,27 @@
+package exercises;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class ObedientRobot {
+
+	public static void main(String[] args) {
+		int shape = JOptionPane.showOptionDialog(null, "Which shape would you like me to draw?", "Obedient Robot", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Circle", "Square", "Triangle" }, null);
+		/*
+		 * Create methods to draw a circle, square and triangle and call the appropriate
+		 * one depending on the user choice.
+		 */
+		
+		Robot alex = new Robot("batman");
+		
+		
+		
+		
+		
+	}}
+	
+	
+
+
